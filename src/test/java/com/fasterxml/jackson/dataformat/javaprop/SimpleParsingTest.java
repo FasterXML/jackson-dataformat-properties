51f446c8cb0d68045f5fd0cb5a4b31f966a5a86a
Tatu Saloranta <tatu.saloranta@iki.fi>
2017-02-21 15:45:39 -0800

    Fixed #6

--- src/test/java/com/fasterxml/jackson/dataformat/javaprop/SimpleParsingTest.java
+++ src/test/java/com/fasterxml/jackson/dataformat/javaprop/SimpleParsingTest.java
@@ -9,10 +9,29 @@ public class SimpleParsingTest extends ModuleTestBase
     private final ObjectMapper MAPPER = mapperForProps();
 
     private final ObjectMapper JSON_MAPPER = new ObjectMapper();
-    
+
     public void testSimpleNonNested() throws Exception {
         _testSimpleNonNested(false);
         _testSimpleNonNested(true);
+    }
+
+    public void testSimpleNested() throws Exception {
+        _testSimpleNested(false);
+        _testSimpleNested(true);
+    }
+
+    public void testSimpleRectangle() throws Exception {
+        _testSimpleRectangle(false);
+        _testSimpleRectangle(true);
+    }
+
+    public void testNonSplittingParsing() throws Exception {
+        JavaPropsSchema schema = JavaPropsSchema.emptySchema()
+                .withoutPathSeparator();
+        Map<?,?> result = MAPPER.readerFor(Map.class)
+                .with(schema)
+                .readValue("a.b.c = 3");
+        assertEquals("{\"a.b.c\":\"3\"}", JSON_MAPPER.writeValueAsString(result));
     }
 
     private void _testSimpleNonNested(boolean useBytes) throws Exception
@@ -27,9 +46,13 @@ public class SimpleParsingTest extends ModuleTestBase
         assertEquals(4, result.getUserImage().length);
     }
 
-    public void testSimpleRectangle() throws Exception {
-        _testSimpleRectangle(false);
-        _testSimpleRectangle(true);
+    private void _testSimpleNested(boolean useBytes) throws Exception
+    {
+        final String INPUT = "comparison.source.database=test\n"
+                +"comparison.target.database=test2\n"
+                ;
+        Map<?,?> result = _mapFrom(MAPPER, INPUT, Map.class, useBytes);
+        assertEquals(1, result.size());
     }
 
     private void _testSimpleRectangle(boolean useBytes) throws Exception
@@ -41,14 +64,5 @@ public class SimpleParsingTest extends ModuleTestBase
         Rectangle result = _mapFrom(MAPPER, INPUT, Rectangle.class, useBytes);
         assertEquals(5, result.bottomRight.x);
         assertEquals(10, result.bottomRight.y);
-    }
-
-    public void testNonSplittingParsing() throws Exception {
-        JavaPropsSchema schema = JavaPropsSchema.emptySchema()
-                .withoutPathSeparator();
-        Map<?,?> result = MAPPER.readerFor(Map.class)
-                .with(schema)
-                .readValue("a.b.c = 3");
-        assertEquals("{\"a.b.c\":\"3\"}", JSON_MAPPER.writeValueAsString(result));
     }
 }
